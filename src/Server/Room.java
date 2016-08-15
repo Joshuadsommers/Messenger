3c7e34ae52ef10926625519906531657a6ca2fcc
Joshua Sommers <js3289@nau.edu>
2016-08-15 13:55:02 -0700

    Commented room logic
    Basic idea for how I wanted to handle rooms layout

--- src/Server/Room.java
+++ src/Server/Room.java
@@ -6,7 +6,20 @@ import java.net.InetAddress;
  * Created by Josh on 8/15/2016.
  */
 public class Room {
-    public Room(InetAddress... addresses){
 
+    // Room is created from server with all InetAddresses as param
+    // When users are added to a room after room is created they will use addUser() and we need to append to addresses
+    public Room(InetAddress... addresses){
+        for( InetAddress a : addresses) {
+            // send message obj to each client in room
+        }
+    }
+
+    public void addUser(InetAddress a) {
+        // Add user to addresses
+    }
+
+    public void removeUser(InetAddress a) {
+        // Remove user from addresses
     }
 }
